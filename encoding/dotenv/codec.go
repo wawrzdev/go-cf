9f5b07962bb11044800708e02edfd6d492d0b9d5
Kris Wawrzyniak <wawrz.dev@gmail.com>
2021-10-22 19:40:55 -0600

    stubbing some codecs

--- encoding/dotenv/codec.go
+++ encoding/dotenv/codec.go
@@ -1,1 +1,21 @@
 package encoding
+
+type Codec struct{}
+
+func (c Codec) Encode(v interface{}) ([]byte, error) {
+	return marshal(v)
+}
+
+func (c Codec) Decode(b []byte, v interface{}) error {
+	return unmarshal(b, v)
+}
+
+//Should marshal the interface into dotenv byte slice
+func marshal(v interface{}) ([]byte, error) {
+	return make([]byte, 0), nil
+}
+
+//Should unmarshal the dotenv byte slice into the interface
+func unmarshal(b []byte, v interface{}) error {
+	return nil
+}
--- encoding/json/codec.go
+++ encoding/json/codec.go
@@ -1,1 +1,13 @@
 package encoding
+
+import "encoding/json"
+
+type Codec struct{}
+
+func (c Codec) Encode(v interface{}) ([]byte, error) {
+	return json.MarshalIndent(v, "", "  ")
+}
+
+func (c Codec) Decode(b []byte, v interface{}) error {
+	return json.Unmarshal(b, v)
+}
